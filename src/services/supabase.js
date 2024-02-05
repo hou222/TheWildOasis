b02b4d0697a25442aad9dbbe4f309aabb55cd6db
Houssam Benlagha <houssambenlagha0@gmail.com>
2024-02-05 23:52:30 +0100

    connecting supabase with this app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://psllteojpckjdkmwrlsd.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzbGx0ZW9qcGNramRrbXdybHNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY0Nzc1MTksImV4cCI6MjAyMjA1MzUxOX0.xNBpD2dC71lYTjHbQ5WbYm-yPrhcplD1o4lTT8eeDv4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
